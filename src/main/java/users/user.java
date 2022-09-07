369efc05f5f8a40c8d2a896a0fd22b13a09ac84b
Redouane Harilla <88410560+AdamHarilla@users.noreply.github.com>
2022-09-07 12:18:27 -0500

    added coonection to db and created tables

---
+++ src/main/java/users/user.java
@@ -0,0 +1,20 @@
+package users;
+
+import javax.management.relation.Role;
+import java.util.Objects;
+
+public class user {
+
+    private string user_id;
+    private string username;
+    private string email;
+    private string password;
+    private string given_name;
+    private string surname;
+    private string is_active;
+    private string role_id;
+    private Role role;
+
+
+
+}
